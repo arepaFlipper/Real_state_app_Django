5c3ade338f57474d962145931fdc24a0ef824f76
cris <ctovar@abexuspr.com>
2022-12-17 06:14:49 -0500

    🍡 comment startproject after build files

--- backend/start.sh
+++ backend/start.sh
@@ -5,8 +5,8 @@ set -o nounset
 
 # pip install --upgrade pip
 # pip install -r ./requirements.txt
-echo $(pwd)
-django-admin startproject real_state .
+# django-admin startproject real_state .
+# python ./manage.py startapp accounts
 python ./manage.py makemigrations
 python ./manage.py migrate
 python ./manage.py runserver 0.0.0.0:8000
