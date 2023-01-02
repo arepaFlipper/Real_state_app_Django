b55c9b6f706811fcdac4af4717295fbc8dd00852
cris <ctovar@abexuspr.com>
2023-01-02 07:28:45 -0500

    🌤️ frontend>404 component

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="m-64 notfound">
+      <h1 className="text-[6.2rem] text-center text-opacity-70 font-extralight mb-16 ">404 Not Found</h1>
+      <p className="text-3xl text-center mb-56">The link you request does not exist on our website.</p>
+    </div>
   )
 }
 
