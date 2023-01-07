0e8b8d4419974a2ad86e88ef9d6f7ceb25416646
cris <ctovar@abexuspr.com>
2023-01-07 06:22:58 -0500

    🎿 part7(14:40) add redux settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,28 @@ import NotFound from './components/NotFound'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { About, Contact, Home, Listings, ListingDetail, Login, SignUp } from "./containers/";
 import './App.css'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/listings' element={<Listings />} />
-          <Route exact path='/listings/:id' element={<ListingDetail />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/signup' element={<SignUp />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Provider store={store} >
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/contact' element={<Contact />} />
+            <Route exact path='/listings' element={<Listings />} />
+            <Route exact path='/listings/:id' element={<ListingDetail />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/signup' element={<SignUp />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
   )
 }
 
